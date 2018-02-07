20c04acf0a3751e6d5b4ebfe57ab4b6396f4ffc0
PatrickGharib <36083375+PatrickGharib@users.noreply.github.com>
2018-02-07 16:44:59 -0700

    Add files via upload

---
+++ partially done parser/src/TNPelement.java
@@ -0,0 +1,27 @@
+public class TNPelement {
+	private String taskOne;
+	private String taskTwo;
+	private long penalty;
+
+	public TNPelement(String taskOne, String taskTwo, long penalty) {
+		this.taskOne = taskOne;
+		this.taskTwo = taskTwo;
+		this.penalty = penalty;
+	}
+
+	public String getTNPtaskOne() {
+		return this.taskOne;
+	}
+
+	public String getTNPtaskTwo() {
+		return this.taskTwo;
+	}
+
+	public long getTNPpenalty() {
+		return this.penalty;
+	}
+
+	protected void setTNPpenalty(long newPenalty) {
+		this.penalty = newPenalty;
+	}
+}
