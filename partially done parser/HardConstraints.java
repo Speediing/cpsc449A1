84233cd9badd98ff7c1b58ed67ea4358d1b01279
thdong <tony.hao.dong@gmail.com>
2018-02-08 20:47:16 -0700

    Update HardConstraints.java

--- partially done parser/HardConstraints.java
+++ partially done parser/HardConstraints.java
@@ -12,10 +12,7 @@
  public class HardConstraints {
 	 
 	 //Constants
-	 MAXASSIGNMENTLENGTH = 7;
-	 
-	 //Variables
-	 boolean isValid = true;
+	 int MAX_ASSIGNMENT_LENGTH = 7;
 	 
 	 //Methods
 	 
@@ -27,19 +24,24 @@
 	  * sends it as well as the index to other boolean methods, to be returned
 	  * and check if the assignment is valid.
 	  */
-	 public static boolean check (ArrayList assignment) {
+	 public static boolean check (ArrayList<String> assignment) {
 		 
-		 int index = assignment.size() - 1;
+		 boolean isValid = true;
 		 
-		 isValid = HardConstraints.forcedPartialAssignment(index, assignment);
+		 int getme = assignment.size() - 1;
+		 
+		 isValid = HardConstraints.forcedPartialAssignment(getme, assignment, isValid);
+		 //System.out.println("isvalid: " + isValid);
 		 
 		 if (isValid == false) return isValid;
 		
-		 isValid = HardConstraints.forbiddenMachine(index, assignment);
+		 isValid = HardConstraints.forbiddenMachine(getme, assignment, isValid);
+		 //System.out.println("isvalid: " + isValid);
 		 
 		 if (isValid == false) return isValid;
 		 
-		 isValid = HardConstraints.tooNearTasks(index, assignment);
+		 isValid = HardConstraints.tooNearTasks(getme, assignment, isValid);
+		 //System.out.println("isvalid: " + isValid);
 		 
 		 return isValid;
 		 
@@ -48,16 +50,21 @@
 	 /*
 	  * Name: forcedPartialAssignment
 	  * Input: an integer by name index, an ArrayList by name assignment
-	  * Ouptut: a boolean value
+	  * Output: a boolean value
 	  * Description: Method takes in the index of the assignment and the assignment needed to be 
 	  * checked and checks if it includes the forced assignments, returns false if it does not.
 	  */
-	 public static boolean forcedPartialAssignment (int index, ArrayList assignment) {
+	 public static boolean forcedPartialAssignment (int GETME, ArrayList<String> assignment, boolean isValid) {
 		 
-		 char taskGiven = assignment.get(index);
+		 String taskGiven = assignment.get(GETME);
+		 //System.out.println("getme: " + GETME + " , taskGiven: " + taskGiven);
 		 
-		 if (fpa.containsValue(index)) {
-			 if (index != fpa.get(taskGiven)) isValid = false;
+		 if (Parser.fpa.containsKey(GETME+1)) {
+			 //System.out.println("hi");
+			 if (!taskGiven.equals(Parser.fpa.get(GETME+1))) {
+				 //System.out.println("hi");
+				 isValid = false;
+			 }
 		 }
 		 
 		 return isValid;
@@ -66,20 +73,26 @@
 	 /*
 	  * Name: forbiddenMachine
 	  * Input: an integer by name index, an ArrayList by name assignment
-	  * Ouptut: a boolean value
+	  * Output: a boolean value
 	  * Description: Method takes in the index of the assignment and the assignment needed to be 
 	  * checked and checks if it includes the forbidden machines, returns false if it does.
 	  */
-	 public static boolean forbiddenMachine (int index, ArrayList assignment) {
+	 public static boolean forbiddenMachine (int getme, ArrayList<String> assignment, boolean isValid) {
 		 
-		 char taskGiven = assignment.get(index);
+		 String taskGiven = assignment.get(getme);
+		 //System.out.println("taskGiven: " + taskGiven + " getme: " + getme);
 		 
-		 for (FMelement element : fmArray) {
-			 task = element.getFMtask();
-			 machine = element.getFMachine();
+		 for (FMelement element : Parser.fmArray) {
+			 String task = element.getFMtask();
+			 int machine = element.getFMachine();
+			 //System.out.println("task: " + task + ", machine: " + machine);
 			 
-			 if (task == taskGiven) {
-				 if (machine == index) isValid = false;
+			 if (task.equals(taskGiven)) {
+				 //System.out.println("ho");
+				 if (machine == (getme+1)) {
+					 //System.out.println("ho");
+					 isValid =  false;
+				 }
 			 }
 		 }
 		 
@@ -89,30 +102,35 @@
 	 /*
 	  * Name: tooNearTasks
 	  * Input: an integer by name index, an ArrayList by name assignment
-	  * Ouptut: a boolean value
+	  * Output: a boolean value
 	  * Description: Method takes in the index of the assignment and the assignment needed to be 
 	  * checked and checks if it includes the too near tasks, returns false if it does.
 	  */
-	 public static boolean tooNearTasks (int index, ArrayList assignment) {
+	 public static boolean tooNearTasks (int getme, ArrayList<String> assignment, boolean isValid) {
 		 
-		 char taskGiven2 = assignment.get(index);
+		 String taskGiven2 = assignment.get(getme);
 		 
-		 int prevIndex = index - 1;
-		 if (prevIndex = -1) return isValid;
-		 
-		 char taskGiven1 = assignment.get(prevIndex);
-		 
-		 for (TNTelement element : tntArray) {
-			 task1 = element.getTNTtaskOne();
-			 task2 = element.getTNTtaskTwo();
-			 
-			 if (task2 == taskGiven2) {
-				 if (task1 == taskGiven1) isValid = false;
-			 }
+		 int prevIndex = getme - 1;
+		 if (prevIndex == -1) {
+			 //System.out.println("he");
+			 return isValid;
 		 }
 		 
+		 String taskGiven1 = assignment.get(prevIndex);
+		 
+		 for (TNTelement element : Parser.tntArray) {
+			 String task1 = element.getTNTtaskOne();
+			 String task2 = element.getTNTtaskTwo();
+			 //System.out.println("task1: " + task1 + ", task2: " + task2);
+			 
+			 if (task2.equals(taskGiven2)) {
+				 if (task1.equals(taskGiven1)) { 
+					 isValid = false;
+				 }
+			 }
+		 }		 
 		 return isValid;
 	 }
+ }
 		 
-		 
-		 		 
+	
