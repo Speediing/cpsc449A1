398c5ab9be201d846c2c96063d9023632a3b28fc
thdong <tony.hao.dong@gmail.com>
2018-02-08 21:05:43 -0700

    Update TNTelement.java

--- partially done parser/TNTelement.java
+++ partially done parser/TNTelement.java
@@ -1,15 +1,17 @@
-public class TNTelement{
-  private String taskOne;
-  private String taskTwo;
+public class TNTelement {
+	private String taskOne;
+	private String taskTwo;
 
-  public TNTelement(String taskOne, String taskTwo){
-  this.taskOne = taskOne;
-  this.taskTwo = taskTwo;
-  }
-  public String getTNTtaskOne(){
-    return this.taskOne;
-  }
-  public String getTNTtaskTwo(){
-    return this.taskTwo;
-  }
+	public TNTelement(String taskOne, String taskTwo) {
+		this.taskOne = taskOne;
+		this.taskTwo = taskTwo;
+	}
+
+	public String getTNTtaskOne() {
+		return this.taskOne;
+	}
+
+	public String getTNTtaskTwo() {
+		return this.taskTwo;
+	}
 }
