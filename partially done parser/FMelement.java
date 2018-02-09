3978c985ca44fb4578c40a368aff5db3cc96eb17
thdong <tony.hao.dong@gmail.com>
2018-02-08 20:46:00 -0700

    Update FMelement.java

--- partially done parser/FMelement.java
+++ partially done parser/FMelement.java
@@ -1,15 +1,17 @@
-public class FMelement{
-  private int machine;
-  private String task;
+public class FMelement {
+	private int machine;
+	private String task;
 
-  public FMelement(int machine, String task){
-    this.machine = machine;
-    this.task = task;
-  }
-  public int getFMachine(){
-    return this.machine;
-  }
-  public String getFMtask(){
-    return this.task;
-  }
+	public FMelement(int machine, String task) {
+		this.machine = machine;
+		this.task = task;
+	}
+
+	public int getFMachine() {
+		return this.machine;
+	}
+
+	public String getFMtask() {
+		return this.task;
+	}
 }
