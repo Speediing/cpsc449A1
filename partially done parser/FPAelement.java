67725a5435561237e5111a9d7dbffc9c001d0f89
thdong <tony.hao.dong@gmail.com>
2018-02-08 21:04:13 -0700

    Update FPAelement.java

--- partially done parser/FPAelement.java
+++ partially done parser/FPAelement.java
@@ -1,17 +1,19 @@
-public class FPAelement{
+public class FPAelement {
 
-  private int machine;
-  private String task;
+	private int machine;
+	private String task;
 
-  public FPAelement(int machine, String task){
-    this.machine = machine;
-    this.task = task;
-  }
-  public int getFPAmachine(){
-    return this.machine;
-  }
-  public String getFPAtask(){
-    return this.task;
-  }
+	public FPAelement(int machine, String task) {
+		this.machine = machine;
+		this.task = task;
+	}
+
+	public int getFPAmachine() {
+		return this.machine;
+	}
+
+	public String getFPAtask() {
+		return this.task;
+	}
 
 }
